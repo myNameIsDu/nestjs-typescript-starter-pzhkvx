9a7f1ff2e226efd58f47b5404cd0cb6d88824f33
myNameIsDu <sunbohaoddd@gmail.com>
2022-07-20 18:27:08 +0800

    3 files modified

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,10 @@ import { Module, NestMiddleware, MiddlewareConsumer } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NextFunction, Request, Response } from 'express';
-
+let count = 1;
 class Middleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    console.log('middleware');
+    console.log(count++);
     next();
   }
 }
@@ -20,10 +20,10 @@ export class AppModule {
   //   consumer.apply(Middleware).forRoutes('/admin/*');
   // }
 
-  configure(consumer: MiddlewareConsumer) {
-    consumer.apply(Middleware).exclude('/hello').forRoutes('/*');
-  }
   // configure(consumer: MiddlewareConsumer) {
-  //   consumer.apply(Middleware).exclude('/api/hello').forRoutes('/*');
+  //   consumer.apply(Middleware).exclude('/hello').forRoutes('/*');
   // }
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(Middleware).exclude('/api/hello').forRoutes('/*');
+  }
 }
